6521fe63572403fedbb68eb836e1971e8164cdf8
Sita Ram Thing <sitaramthing667@gmail.com>
2023-07-20 10:15:07 +0545

    create a api callinastance class, ApiServices interface and Domain class

---
+++ app/src/main/java/com/sitaram/gameyo/features/api/ApiCallInstance.kt
@@ -0,0 +1,41 @@
+package com.sitaram.gameyo.features.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiCallInstance {
+
+    companion object {
+        private var retrofit: Retrofit? = null
+        private const val BASE_URL = "https://www.freetogame.com/api/"
+
+        // create the instance of retrofit
+        @Synchronized
+        fun getRetrofitInstance(): Retrofit? {
+            if (retrofit == null) {
+                // create the object of httpLoggingInterceptor
+                val httpLoggingInterceptor = HttpLoggingInterceptor()
+                httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+                // create object of okHttpClient
+                val okHttpClient: OkHttpClient =
+                    OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+
+                // create an object of the retrofit
+                retrofit = Retrofit.Builder().baseUrl(BASE_URL).client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create()) // converter
+                    .build()
+            }
+            // return the retrofit
+            return retrofit
+        }
+
+        // call retrofit instance
+        @Synchronized
+        fun getAPIServiceInstance(): ApiServices? {
+            return getRetrofitInstance()?.create(ApiServices::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/sitaram/gameyo/features/api/ApiServices.kt
@@ -0,0 +1,13 @@
+package com.sitaram.gameyo.features.api
+
+import com.sitaram.composeapp.features.game.pojo.GameItems
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiServices {
+
+    // local server data
+    @GET("games")
+    fun getGames(): Call<List<GameItems>>
+}
