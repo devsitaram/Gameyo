4c19a28893cf46df694e914be339d20bfe6ee810
Sita Ram Thing <sitaramthing667@gmail.com>
2023-07-20 13:08:52 +0545

    create a new message features and design on ui for message screen

--- app/src/main/java/com/sitaram/gameyo/features/main/Main.kt
+++ app/src/main/java/com/sitaram/gameyo/features/main/Main.kt
@@ -26,6 +26,7 @@ import com.sitaram.composeapp.features.game.GameScreen
 import com.sitaram.gameyo.features.forgotpassword.ForgotPasswordViewScreen
 import com.sitaram.gameyo.features.home.HomeViewScreen
 import com.sitaram.gameyo.features.login.LoginViewScreen
+import com.sitaram.gameyo.features.message.MessageViewScreen
 import com.sitaram.gameyo.features.profile.ProfileViewScreen
 import com.sitaram.gameyo.features.register.SignUpViewScreen
 import com.sitaram.gameyo.features.setting.SettingViewScreen
@@ -114,7 +115,7 @@ fun ViewOfMainPage() {
         NavHost(navController, startDestination = ScreenItem.Home.route, Modifier.padding(innerPadding)) {
             composable(ScreenItem.Home.route) { HomeViewScreen() }
             composable(ScreenItem.Profile.route) { ProfileViewScreen() }
-            composable(ScreenItem.Message.route) { MessageScreen() }
+            composable(ScreenItem.Message.route) { MessageViewScreen() }
             composable(ScreenItem.Game.route) { GameScreen() }
             composable(ScreenItem.Setting.route) { SettingViewScreen() }
         }
